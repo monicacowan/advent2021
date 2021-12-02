cbea93f16aa9af421639b71cc2f750d7fbdea8a8
Monica_Cowan <monica@outerlabs.io>
2021-12-02 11:12:58 -0800

    December 2

--- main.go
+++ main.go
@@ -5,12 +5,15 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Welcome to advent of code!")
 	dec01a, dec01b := December01()
 	fmt.Printf("the answer to dec 01 is: %v, %v\n", dec01a, dec01b)
+	dec02a := December02()
+	fmt.Printf("the answer to dec 02 is: %v\n", dec02a)
 }
 
 func December01() (int, int) {
@@ -19,7 +22,6 @@ func December01() (int, int) {
 	if err != nil {
 		panic("unable to open file :(")
 	}
-	fmt.Printf("successfully opened file!")
 	 // read into an array
 	 var depths []int
 
@@ -31,8 +33,6 @@ func December01() (int, int) {
 		}
 		depths = append(depths, num)
 	}
-
-	fmt.Printf("the file is %v lines long \n", len(depths))
 
 	countA := 0
 	// part a
@@ -66,4 +66,53 @@ func December01() (int, int) {
 		panic("unable to close file :(")
 	}
 	return countA, countB
+}
+
+func December02() int {
+	// open the file
+	file, err := os.Open("december02.txt")
+	if err != nil {
+		panic("couldn't open the file :(\n")
+	}
+
+	type movement struct{
+		direction string
+		distance int
+	}
+	var movements []movement
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		 m := strings.Fields(scanner.Text()) // will be in the format: direction distance
+
+		 // convert distance to int32
+		 dist, err := strconv.Atoi(m[1])
+		 if err != nil {
+			 panic("couldn't convert string to int")
+		 }
+
+		 movements = append(movements, movement{
+			 direction: m[0],
+			 distance:  dist,
+		 })
+	}
+
+	// initialize all three categories
+	forward := 0
+	up := 0
+	down := 0
+
+	for _, mvmt := range movements {
+		switch mvmt.direction {
+		case "forward":
+			forward += mvmt.distance
+		case "up":
+			up += mvmt.distance
+		case "down":
+			down += mvmt.distance
+		}
+	}
+
+	depth := down - up
+
+	return depth * forward
 }
